2143b12ba62d61d1e350c3e50da0245ea4edce99
Samuel Lair <lair001@gmail.com>
2018-06-18 00:27:29 -0400

    Implement exercise 1-4

---
+++ chapter-1/section-2/1-4/celsius_fahr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+	for celsius = 0, 20, ..., 300; floating point version */
+
+main()
+{
+	float celsius, fahr;
+	int lower, upper, step;
+
+	lower = -20; /* lower limit of temperature table */
+	upper = 150; /* upper limit */
+	step = 10; /* step size */
+
+	celsius = lower;
+	printf("celsius  fahr\n");
+	while (celsius <= upper) {
+		fahr = (9.0/5.0) * celsius + 32.0;
+		printf("%7.0f  %4.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
