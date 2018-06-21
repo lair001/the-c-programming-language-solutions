ad6fe2f6634ebf0ef0f235e6d896868439b0f7b9
Samuel Lair <lair001@gmail.com>
2018-06-20 22:41:07 -0400

    Implement 1.5.3 example 1

---
+++ chapters/1/sections/5/subsections/3/example_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* count lines in input */
+
+int main()
+{
+	int c, n1;
+
+	n1 = 0;
+	while ((c = getchar()) != EOF)
+		if (c == '\n')
+			++n1;
+	printf("%d\n", n1);
+
+	return 0;
+}
