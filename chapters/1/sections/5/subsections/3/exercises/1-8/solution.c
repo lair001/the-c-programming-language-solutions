810007da0ad92ff668758c8ead4a9d452d30df9d
Samuel Lair <lair001@gmail.com>
2018-06-20 22:52:31 -0400

    Implement exercise 1-8

---
+++ chapters/1/sections/5/subsections/3/exercises/1-8/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int c, n1;
+
+	n1 = 0;
+
+	while ((c = getchar()) != EOF)
+		if (c == '\n' || c == '\t' || c == ' ')
+			++n1;
+	printf("%d\n", n1);
+
+	return 0;
+}
