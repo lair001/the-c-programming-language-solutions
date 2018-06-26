7d6806b8ccf91ffee7666f29351574a4651c29c5
Samuel Lair <lair001@gmail.com>
2018-06-26 01:08:56 -0400

    Add cl arg validation to exercise 1-20

--- chapters/1/sections/10/exercises/1-20/solution.c
+++ chapters/1/sections/10/exercises/1-20/solution.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define DEFAULT_TAB_SIZE 4
+
+int parseTabSize(char arg[]) {
+	int i = 1, isInt = isdigit(arg[0]) ? 1 : 0;
+	if (isInt) {
+		while(isdigit(arg[i])) ++i;
+		isInt = arg[i] == '\0' ? 1 : 0;
+	}
+
+	return isInt ? atoi(arg) : DEFAULT_TAB_SIZE;
+}
 
 int main(int argc, char *argv[])
 {
 	int c, currentLineLength = 0,
-		tabSize = argc < 2 ? DEFAULT_TAB_SIZE : atoi(argv[1]);
+		tabSize = argc < 2 ? DEFAULT_TAB_SIZE : parseTabSize(argv[1]);
 
 	while((c = getchar()) != EOF) {
 		if (c == '\t') {
