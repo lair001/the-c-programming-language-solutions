5aa39276cfdf83467893c3630911ba2a842d7268
Samuel Lair <lair001@gmail.com>
2018-06-30 23:31:46 -0400

    Further improve exercise 1-23 tests

--- chapters/1/sections/10/exercises/1-23/test.c
+++ chapters/1/sections/10/exercises/1-23/test.c
@@ -3,10 +3,13 @@
 // should not appear 003
 /* should not appear 004
  * should not appear 005
-should not appear 006 **/ should appear 007
-/* should not appear 008
-should not appear 009 // should not appear 010
-should not appear 011 */ should appear 012
-should appear 013
-"/*" should appear 014 "*/"
-'should appear 015 /*' should not appear 016 '*/ should appear 017'
+should not appear 006 **/ should appear 101
+/* should not appear 007
+should not appear 008 // should not appear 009
+should not appear 010 */ should appear 102
+should appear 103
+"/*" should appear 104 "*/"
+'should appear 105 /*' should not appear 011 '*/ should appear 106'
+
+should not appear indexes: 001-011
+should appear indexes: 101-106
