7617aca7c9d0abe4535d0b85a1cca7715e935a0a
Samuel Lair <lair001@gmail.com>
2018-06-30 22:16:11 -0400

    Improve tests for exercise 1-23

--- chapters/1/sections/10/exercises/1-23/test.c
+++ chapters/1/sections/10/exercises/1-23/test.c
@@ -1,9 +1,12 @@
 /* Tests */
-// Ipsum //
-// Lorem
-/* Ipsum
- * Lorem
- **/
-/* Ipsum
-lorem // add
-florum */
+// should not appear 001 // should not appear 002
+// should not appear 003
+/* should not appear 004
+ * should not appear 005
+should not appear 006 **/ should appear 007
+/* should not appear 008
+should not appear 009 // should not appear 010
+should not appear 011 */ should appear 012
+should appear 013
+"/*" should appear 014 "*/"
+'should appear 015 /*' should not appear 016 '*/ should appear 017'
