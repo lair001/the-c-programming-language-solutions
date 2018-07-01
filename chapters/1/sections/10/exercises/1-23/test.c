df01cc3370d0978bf1ab02ad683aab1589ce6065
Samuel Lair <lair001@gmail.com>
2018-07-01 01:26:35 -0400

    Add comments explaining exercise 1-23 tests

--- chapters/1/sections/10/exercises/1-23/test.c
+++ chapters/1/sections/10/exercises/1-23/test.c
@@ -1,4 +1,7 @@
-/* Tests */
+/* Tests 
+ * This is not a compilable program.
+ * This is test input for solution.c */
+
 // should not appear 001 // should not appear 002
 // should not appear 003
 /* should not appear 004
@@ -11,5 +14,9 @@ should appear 103
 "/*" should appear 104 "*/"
 'should appear 105 /*' should not appear 011 '*/ should appear 106'
 
+/* These are the index ranges.
+ * The should not appear labels should not appear.
+ * The should appear labels should appear for the
+ * entirety of their respective index range. */
 should not appear indexes: 001-011
 should appear indexes: 101-106
