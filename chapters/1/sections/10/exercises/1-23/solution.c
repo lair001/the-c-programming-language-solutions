e9fcb934a4843040ba1bb4fcf84d1d9031a26597
Samuel Lair <lair001@gmail.com>
2018-07-02 21:34:48 -0400

    Fix bug in exercise 1-23
    Char literals were not handled properly.  In particular, character literal
    '"' would trigger the beginning or end of a string.

--- chapters/1/sections/10/exercises/1-23/solution.c
+++ chapters/1/sections/10/exercises/1-23/solution.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
 
-char charQueue[2];
+char charQueue[5];
+int isCharLiteral = 0;
 int isInsideDoubleQuotes = 0;
 int isInsideSingleLineComment = 0;
 int isInsideMultiLineComment = 0;
 int currentLineLength = 0;
 int currentLineHasComment = 0;
+
+int isInsideComment(void);
+void putChar(int c);
+void enqueue(int c);
+void clear(void);
+void checkCharQueue(void);
+void processChar(int c);
+int charQueueHasNonNullChar(void);
+void flushCharQueue(void);
+
+int main() {
+	int c;
+
+	clear();
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\n') {
+			flushCharQueue();
+			if (currentLineLength > 0 || !currentLineHasComment) {
+				putchar('\n');
+			}
+			currentLineLength = 0;
+			isInsideSingleLineComment = 0;
+			isCharLiteral = 0;
+			currentLineHasComment = 0;
+		} else {
+			processChar(c);
+		}
+
+	}
+	flushCharQueue();
+
+	return 0;
+}
 
 int isInsideComment(void) {
 	return isInsideSingleLineComment || isInsideMultiLineComment;
@@ -21,41 +56,49 @@ void putChar(int c) {
 void enqueue(int c) {
 	putChar(charQueue[0]);
 	charQueue[0] = charQueue[1];
-	charQueue[1] = c;
+	charQueue[1] = charQueue[2];
+	charQueue[2] = charQueue[3];
+	charQueue[3] = charQueue[4];
+	charQueue[4] = c;
 }
 
 void clear(void) {
 	charQueue[0] = '\0';
-        charQueue[1] = '\0';
-}
-
-void putAndClear(void) {
-	putChar(charQueue[0]);
-	putChar(charQueue[1]);
-	clear();
+	charQueue[1] = '\0';
+	charQueue[2] = '\0';
+	charQueue[3] = '\0';
+	charQueue[4] = '\0';
 }
 
 void checkCharQueue(void) {
-	if (charQueue[0] == '/') { 
+	if (isCharLiteral) {
+		if (charQueue[2] == '\'') isCharLiteral = 0;
+	}
+	else if (charQueue[0] == '/') {
 		if (charQueue[1] == '/')
-			isInsideSingleLineComment = 
+			isInsideSingleLineComment =
 				isInsideMultiLineComment
 				|| isInsideDoubleQuotes ? 0 : 1;
 		else if (charQueue[1] == '*')
 			isInsideMultiLineComment =
 				isInsideSingleLineComment
 				|| isInsideDoubleQuotes ? 0 : 1;
-	}	
+	}
 	else if (charQueue[0] == '*') {
 		if (charQueue[1] == '/') {
 			if (isInsideMultiLineComment) {
-				clear();
 				isInsideMultiLineComment = 0;
+				charQueue[0] = '\0';
+				charQueue[1] = '\0';
 			}
 		}
 	}
 	else if (charQueue[0] != '\\') {
-		if (charQueue[1] == '"')
+		if (charQueue[1] == '\'') {
+			if (charQueue[3] == '\''
+					|| (charQueue[2] == '\\' && charQueue[4] == '\'')) isCharLiteral = 1;
+		}
+		else if (charQueue[1] == '"')
 			isInsideDoubleQuotes =
 				isInsideDoubleQuotes
 				|| isInsideComment() ? 0 : 1;
@@ -63,27 +106,18 @@ void checkCharQueue(void) {
 	if (isInsideComment()) currentLineHasComment = 1;
 }
 
-int main() {
-	int c;
+void processChar(int c) {
+	enqueue(c);
+	checkCharQueue();
+}
 
-	clear();
-
-	while ((c = getchar()) != EOF) {
-		if (c == '\n') {
-			putAndClear();
-			if (currentLineLength > 0 || !currentLineHasComment) {
-				putchar('\n');
-			}
-			currentLineLength = 0;
-			isInsideSingleLineComment = 0;
-			currentLineHasComment = 0;
-		} else {
-			enqueue(c);
-			checkCharQueue();
-		}
-
-	}
-
+int charQueueHasNonNullChar(void) {
+	for (int i = 0; i < 5; i++) if (charQueue[i] != '\0') return 1;
 	return 0;
 }
 
+void flushCharQueue(void) {
+	while(charQueueHasNonNullChar()) {
+		processChar('\0');
+	}
+}
