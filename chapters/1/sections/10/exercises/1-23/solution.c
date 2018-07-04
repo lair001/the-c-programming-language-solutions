43630988112d70f73b9bcfa7b060c7abd76e1623
Samuel Lair <lair001@gmail.com>
2018-07-04 03:00:09 -0400

    Reduce cyclomatic complexity of checkCharQueue ...
    ... in exercise 1-23

--- chapters/1/sections/10/exercises/1-23/solution.c
+++ chapters/1/sections/10/exercises/1-23/solution.c
@@ -12,6 +12,16 @@ int isInsideComment(void);
 void putChar(int c);
 void enqueue(int c);
 void clear(void);
+int doesCharQueueHaveSingleLineCommentOpening(void);
+void openSingleLineComment(void);
+int doesCharQueueHaveMultiLineCommentOpening(void);
+void openMultiLineComment(void);
+int doesCharQueueHaveMultiLineCommentClosing(void);
+void closeMultiLineComment(void);
+int doesCharQueueHaveCharLiteral(void);
+void openCharLiteral(void);
+int doesCharQueueHaveDoubleQuotes(void);
+void openOrCloseStringLiteral(void);
 void checkCharQueue(void);
 void processChar(int c);
 int charQueueHasNonNullChar(void);
@@ -70,39 +80,69 @@ void clear(void) {
 	charQueue[4] = '\0';
 }
 
+int doesCharQueueHaveSingleLineCommentOpening(void) {
+	return charQueue[0] == '/' && charQueue[1] == '/';
+}
+
+void openSingleLineComment(void) {
+	isInsideSingleLineComment =
+		isInsideMultiLineComment
+		|| isInsideDoubleQuotes ? 0 : 1;
+}
+
+int doesCharQueueHaveMultiLineCommentOpening(void) {
+	return charQueue[0] == '/' && charQueue[1] == '*';
+}
+
+void openMultiLineComment(void) {
+	isInsideMultiLineComment =
+		isInsideSingleLineComment
+		|| isInsideDoubleQuotes ? 0 : 1;
+}
+
+int doesCharQueueHaveMultiLineCommentClosing(void) {
+	return charQueue[0] == '*' && charQueue[1] == '/';
+}
+
+void closeMultiLineComment(void) {
+	if (isInsideMultiLineComment) {
+		isInsideMultiLineComment = 0;
+		charQueue[0] = '\0';
+		charQueue[1] = '\0';
+	}
+}
+
+int doesCharQueueHaveCharLiteral(void) {
+	return charQueue[0] != '\\'
+			&& charQueue[1] == '\''
+			&& (charQueue[3] == '\''
+				|| (charQueue[2] == '\\' && charQueue[4] == '\''));
+}
+
+void openCharLiteral(void) {
+	if (!isInsideComment()) isCharLiteral = 1;
+}
+
+int doesCharQueueHaveDoubleQuotes(void) {
+	return charQueue[0] != '\\' && charQueue[1] == '"';
+}
+
+void openOrCloseStringLiteral(void) {
+	isInsideDoubleQuotes =
+		isInsideDoubleQuotes
+		|| isInsideComment() ? 0 : 1;
+}
+
 void checkCharQueue(void) {
 	if (isCharLiteral) {
 		if (charQueue[2] == '\'') isCharLiteral = 0;
 	}
-	else if (charQueue[0] == '/') {
-		if (charQueue[1] == '/')
-			isInsideSingleLineComment =
-				isInsideMultiLineComment
-				|| isInsideDoubleQuotes ? 0 : 1;
-		else if (charQueue[1] == '*')
-			isInsideMultiLineComment =
-				isInsideSingleLineComment
-				|| isInsideDoubleQuotes ? 0 : 1;
-	}
-	else if (charQueue[0] == '*') {
-		if (charQueue[1] == '/') {
-			if (isInsideMultiLineComment) {
-				isInsideMultiLineComment = 0;
-				charQueue[0] = '\0';
-				charQueue[1] = '\0';
-			}
-		}
-	}
-	else if (charQueue[0] != '\\') {
-		if (charQueue[1] == '\'') {
-			if (charQueue[3] == '\''
-					|| (charQueue[2] == '\\' && charQueue[4] == '\'')) isCharLiteral = 1;
-		}
-		else if (charQueue[1] == '"')
-			isInsideDoubleQuotes =
-				isInsideDoubleQuotes
-				|| isInsideComment() ? 0 : 1;
-	}
+	else if (doesCharQueueHaveSingleLineCommentOpening()) openSingleLineComment();
+	else if (doesCharQueueHaveMultiLineCommentOpening()) openMultiLineComment();
+	else if (doesCharQueueHaveMultiLineCommentClosing()) closeMultiLineComment();
+	else if (doesCharQueueHaveCharLiteral()) openCharLiteral();
+	else if (doesCharQueueHaveDoubleQuotes()) openOrCloseStringLiteral();
+
 	if (isInsideComment()) currentLineHasComment = 1;
 }
 
