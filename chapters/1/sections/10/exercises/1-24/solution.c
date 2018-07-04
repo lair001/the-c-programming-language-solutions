45edc25a87dca65269072dbca42960b5e14bb427
Samuel Lair <lair001@gmail.com>
2018-07-04 19:17:47 -0400

    Reduce cyclomatic complexity of checkCharQueue ...
    ... in exercise 1-24.  Also renamed some variables to parallel
    exercise 1-23.

--- chapters/1/sections/10/exercises/1-24/solution.c
+++ chapters/1/sections/10/exercises/1-24/solution.c
@@ -6,9 +6,9 @@
 
 char charQueue[5];
 int isCharLiteral = 0;
-int isInsideDoubleQuotes = 0;
-int isInsideSingleLineComment = 0;
-int isInsideMultiLineComment = 0;
+int isStringLiteral = 0;
+int isSingleLineComment = 0;
+int isMultiLineComment = 0;
 int currentLineNumber = 1;
 
 /* Index 0 contains the length of the list.
@@ -30,10 +30,20 @@ int processChar(int c);
 int charQueueHasNonNullChar(void);
 int flushCharQueue(void);
 void clearLists(void);
-int checkCharQueueForParenBraceOrBracket(void);
-int isInsideComment(void);
+int isComment(void);
 void enqueueInCharQueue(int c);
 void clearCharQueue(void);
+int doesCharQueueHaveSingleLineCommentOpening(void);
+void openSingleLineComment(void);
+int doesCharQueueHaveMultiLineCommentOpening(void);
+void openMultiLineComment(void);
+int doesCharQueueHaveMultiLineCommentClosing(void);
+void closeMultiLineComment(void);
+int doesCharQueueHaveCharLiteral(void);
+void openCharLiteral(void);
+int doesCharQueueHaveDoubleQuotes(void);
+void openOrCloseStringLiteral(void);
+int shouldCheckCharQueueForParenBraceOrBracket(void);
 int checkCharQueue(void);
 int pushCurrentLineNumberOn(int lineNumList[]);
 int whenMaxLevelExceeded(char type[], int maxLevel);
@@ -54,7 +64,7 @@ int main() {
 	while ((c = getchar()) != EOF) {
 		if (c == '\n') {
 			if ((hasError = flushCharQueue()) != 0) return hasError;
-			isInsideSingleLineComment = 0;
+			isSingleLineComment = 0;
 			isCharLiteral = 0;
 			++currentLineNumber;
 		} else {
@@ -92,8 +102,8 @@ void clearLists(void) {
 	for (int i = 0; i < MAX_BRACE_LEVEL + 1; ++i) openBrace[i] = 0;
 }
 
-int isInsideComment(void) {
-	return isInsideSingleLineComment || isInsideMultiLineComment;
+int isComment(void) {
+	return isSingleLineComment || isMultiLineComment;
 }
 
 void enqueueInCharQueue(int c) {
@@ -112,44 +122,75 @@ void clearCharQueue(void) {
 	charQueue[4] = '\0';
 }
 
+int doesCharQueueHaveSingleLineCommentOpening(void) {
+	return charQueue[0] == '/' && charQueue[1] == '/';
+}
+
+void openSingleLineComment(void) {
+	isSingleLineComment =
+		isMultiLineComment
+		|| isStringLiteral ? 0 : 1;
+}
+
+int doesCharQueueHaveMultiLineCommentOpening(void) {
+	return charQueue[0] == '/' && charQueue[1] == '*';
+}
+
+void openMultiLineComment(void) {
+	isMultiLineComment =
+		isSingleLineComment
+		|| isStringLiteral ? 0 : 1;
+}
+
+int doesCharQueueHaveMultiLineCommentClosing(void) {
+	return charQueue[0] == '*' && charQueue[1] == '/';
+}
+
+void closeMultiLineComment(void) {
+	if (isMultiLineComment) {
+		clearCharQueue();
+		isMultiLineComment = 0;
+	}
+}
+
+int doesCharQueueHaveCharLiteral(void) {
+	return charQueue[0] != '\\'
+			&& charQueue[1] == '\''
+			&& (charQueue[3] == '\''
+				|| (charQueue[2] == '\\' && charQueue[4] == '\''));
+}
+
+void openCharLiteral(void) {
+	if (!isComment()) isCharLiteral = 1;
+}
+
+int doesCharQueueHaveDoubleQuotes(void) {
+	return charQueue[0] != '\\' && charQueue[1] == '"';
+}
+
+void openOrCloseStringLiteral(void) {
+	isStringLiteral =
+		isStringLiteral
+		|| isComment() ? 0 : 1;
+}
+
+int shouldCheckCharQueueForParenBraceOrBracket(void) {
+	return charQueue[0] != '\\' && !isStringLiteral && !isComment();
+}
+
 int checkCharQueue(void) {
 	int hasError = 0;
 
 	if (isCharLiteral) {
 		if (charQueue[2] == '\'') isCharLiteral = 0;
 	}
-	else if (charQueue[0] == '/') {
-		if (charQueue[1] == '/')
-			isInsideSingleLineComment =
-				isInsideMultiLineComment
-				|| isInsideDoubleQuotes ? 0 : 1;
-		else if (charQueue[1] == '*')
-			isInsideMultiLineComment =
-				isInsideSingleLineComment
-				|| isInsideDoubleQuotes ? 0 : 1;
-	}
-	else if (charQueue[0] == '*') {
-		if (charQueue[1] == '/') {
-			if (isInsideMultiLineComment) {
-				clearCharQueue();
-				isInsideMultiLineComment = 0;
-			}
-		}
-	}
-	else if (charQueue[0] != '\\') {
-		if (charQueue[1] == '\'') {
-			if (charQueue[3] == '\''
-					|| (charQueue[2] == '\\' && charQueue[4] == '\'')) isCharLiteral = 1;
-		}
-		else if (charQueue[1] == '"') {
-			isInsideDoubleQuotes =
-				isInsideDoubleQuotes
-				|| isInsideComment() ? 0 : 1;
-		}
-		else if (!isInsideDoubleQuotes && !isInsideComment()) {
-			hasError = checkCharQueueForParenBraceOrBracket();
-		}
-	}
+	else if (doesCharQueueHaveSingleLineCommentOpening()) openSingleLineComment();
+	else if (doesCharQueueHaveMultiLineCommentOpening()) openMultiLineComment();
+	else if (doesCharQueueHaveMultiLineCommentClosing()) closeMultiLineComment();
+	else if (doesCharQueueHaveCharLiteral()) openCharLiteral();
+	else if (doesCharQueueHaveDoubleQuotes()) openOrCloseStringLiteral();
+	else if (shouldCheckCharQueueForParenBraceOrBracket()) hasError = checkCharQueueForParenBraceOrBracket();
+
 	return hasError;
 }
 
